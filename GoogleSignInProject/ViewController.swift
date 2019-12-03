940c82c362686451b8feee7a820f08f6d7180aa3
城間海月 <mizushiro426426@gmail.com>
2019-12-03 15:19:39 +0800

    ログイン、承認画が完成

--- GoogleSignInProject/ViewController.swift
+++ GoogleSignInProject/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        GIDSignIn.sharedInstance()?.delegate = self
+        GIDSignIn.sharedInstance()?.presentingViewController = self
     }
 
 
 }
 
+extension ViewController: GIDSignInDelegate {
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        if let error = error {
+            // errorfがnilでない場合（エラーが存在する場合）
+            print(error.localizedDescription)
+            // 処理を中断
+            return
+        }
+        
+        guard let authentication = user.authentication else {
+            // 認証情報が取れなければ、処理を中断
+            return
+        }
+        
+        let credenital = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        
+        Auth.auth().signIn(with: credenital) { (result, error) in
+            
+            if let error = error {
+                // エラーがある場合
+                print(error.localizedDescription)
+            } else {
+                // ログインに成功した場合
+                print("ログインに成功")
+                self.performSegue(withIdentifier: "toHome", sender: nil)
+            }
+            
+        }
+        
+    }
+}
